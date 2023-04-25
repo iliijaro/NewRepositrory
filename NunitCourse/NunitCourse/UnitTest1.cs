07e647240ca9130547308e25f5353006da00d19a
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-04-26 01:32:34 +0200

    The main branch is created

---
+++ NunitCourse/NunitCourse/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+
+        public void Setup()
+        {
+        }
+
+        [Test]
+
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
