56e108256bb3a1571655c42d37ee6f564b006593
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-04-26 01:35:19 +0200

    The Nunit tests for Calculator are done

---
+++ NunitCourse/NunitCourse/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NunitCourse
+{
+    public class Calculator
+    {
+        public int Summ(int x, int y) => x + y;
+
+        public int Minus(int x, int y) => x - y;
+
+        public int Divide(int x, int y) => x / y;
+
+        public int Multiply(int x, int y) => x * y;
+    }
+}
--- NunitCourse/NunitCourse/UnitTest1.cs
+++ NunitCourse/NunitCourse/UnitTest1.cs
@@ -1,20 +1,73 @@
-﻿using NUnit.Framework;
-
-namespace NUnitTest
+﻿using NunitCourse;
+namespace Calculator_Test
 {
+    [TestFixture]
+
     public class Tests
     {
-        [SetUp]
+        Calculator calculator = new();
 
-        public void Setup()
+        [SetUp]
+        public void SetupForEeachMethod()
         {
+            calculator = new Calculator();
         }
 
-        [Test]
-
-        public void Test1()
+        [Test, Description("Calculator's addition function testing")]
+        [Category("Calculator Test")]
+        [TestCase(6, 9, ExpectedResult = 15)]
+        [TestCase(0, 3, ExpectedResult = 3)]
+        [TestCase(-8, -7, ExpectedResult = -15)]
+        public int SummTest(int operand_1, int operand_2)
         {
-            Assert.Pass();
+            var actualResult = calculator.Summ(operand_1, operand_2);
+            return actualResult;
+        }
+
+        [Test, Description("Calculator's subtraction function testing")]
+        [Category("Calculator Test")]
+        public void MinusTest(
+            [Values(22)] int operand_1,
+            [Range(1, 10, 3)] int operand_2)
+        {
+            var actualResult = calculator.Minus(operand_1, operand_2);
+            var expectedResult = operand_1 - operand_2;
+
+            Assert.That(expectedResult, Is.EqualTo(actualResult), $"Minus result: {operand_1} - {operand_2} = {actualResult}");
+        }
+
+        [Test, Description("Calculator's divide function testing")]
+        [Category("Calculator Test")]
+        [Retry(2)]
+        public void DivideTest(
+            [Values(16, 0)] int operand_1,
+            [Values(2, 4)] int operand2)
+        {
+            var actualResult = calculator.Divide(operand_1, operand2);
+            Console.WriteLine($"Operand 1 = {operand_1}, operand 2 = {operand2}");
+            Console.WriteLine($"Divide result is {actualResult}");
+
+            Assert.That(actualResult != 0, "The actual result equals 0");
+        }
+
+        [Test, Description("Calculator's multiply function testing")]
+        [Category("Calculator Test")]
+        public void MultiplyTest(
+            [Values(5)] int operand_1,
+            [Random(1, 10, 3)] int operand_2)
+        {
+            var actualResult = calculator.Multiply(operand_1, operand_2);
+            var expectedResult = operand_1 * operand_2;
+            Console.WriteLine($"Operand 1 = {operand_1}, operand 2 = {operand_2}");
+            Console.WriteLine($"Multiply result is {actualResult}");
+
+            Assert.AreEqual(expectedResult, actualResult, $"Multiply{operand_1} * {operand_2} = {actualResult}");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("<End of the Test>");
         }
     }
 }
